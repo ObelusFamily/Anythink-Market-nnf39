a65c0fab110de39692aaeddf369fcbdd9ce76e09
FCdSP <petribu.felipe@gmail.com>
2022-08-18 00:20:20 -0300

    added 100 users, produts and comments

--- backend/seeds.sh
+++ backend/seeds.sh
@@ -1,3 +1,104 @@
 #!/bin/sh
 
 python3 ./app/db/seeds.py
+
+User1	 	Solar clothe drying	 	Very useful	 	but expensive
+User2	 	White print toner	 	Not so useful	 	and expensive
+User3	 	Glass hammer	 	Very useful	 	and fair price
+User4	 	Wireless wires	 	Very useful	 	but expensive
+User5	 	Power balance 	 	Not so useful	 	and expensive
+User6	 	Solar clothe drying	 	Very useful	 	and fair price
+User7	 	White print toner	 	Very useful	 	but expensive
+User8	 	Glass hammer	 	Not so useful	 	and expensive
+User9	 	Wireless wires	 	Very useful	 	and fair price
+User10	 	Power balance 	 	Very useful	 	but expensive
+User11	 	Solar clothe drying	 	Not so useful	 	and expensive
+User12	 	White print toner	 	Very useful	 	and fair price
+User13	 	Glass hammer	 	Very useful	 	but expensive
+User14	 	Wireless wires	 	Not so useful	 	and expensive
+User15	 	Power balance 	 	Very useful	 	and fair price
+User16	 	Solar clothe drying	 	Very useful	 	but expensive
+User17	 	White print toner	 	Not so useful	 	and expensive
+User18	 	Glass hammer	 	Very useful	 	and fair price
+User19	 	Wireless wires	 	Very useful	 	but expensive
+User20	 	Power balance 	 	Not so useful	 	and expensive
+User21	 	Solar clothe drying	 	Very useful	 	and fair price
+User22	 	White print toner	 	Very useful	 	but expensive
+User23	 	Glass hammer	 	Not so useful	 	and expensive
+User24	 	Wireless wires	 	Very useful	 	and fair price
+User25	 	Power balance 	 	Very useful	 	but expensive
+User26	 	Solar clothe drying	 	Not so useful	 	and expensive
+User27	 	White print toner	 	Very useful	 	and fair price
+User28	 	Glass hammer	 	Very useful	 	but expensive
+User29	 	Wireless wires	 	Not so useful	 	and expensive
+User30	 	Power balance 	 	Very useful	 	and fair price
+User31	 	Solar clothe drying	 	Very useful	 	but expensive
+User32	 	White print toner	 	Not so useful	 	and expensive
+User33	 	Glass hammer	 	Very useful	 	and fair price
+User34	 	Wireless wires	 	Very useful	 	but expensive
+User35	 	Power balance 	 	Not so useful	 	and expensive
+User36	 	Solar clothe drying	 	Very useful	 	and fair price
+User37	 	White print toner	 	Very useful	 	but expensive
+User38	 	Glass hammer	 	Not so useful	 	and expensive
+User39	 	Wireless wires	 	Very useful	 	and fair price
+User40	 	Power balance 	 	Very useful	 	but expensive
+User41	 	Solar clothe drying	 	Not so useful	 	and expensive
+User42	 	White print toner	 	Very useful	 	and fair price
+User43	 	Glass hammer	 	Very useful	 	but expensive
+User44	 	Wireless wires	 	Not so useful	 	and expensive
+User45	 	Power balance 	 	Very useful	 	and fair price
+User46	 	Solar clothe drying	 	Very useful	 	but expensive
+User47	 	White print toner	 	Not so useful	 	and expensive
+User48	 	Glass hammer	 	Very useful	 	and fair price
+User49	 	Wireless wires	 	Very useful	 	but expensive
+User50	 	Power balance 	 	Not so useful	 	and expensive
+User51	 	Solar clothe drying	 	Very useful	 	but expensive
+User52	 	White print toner	 	Not so useful	 	and expensive
+User53	 	Glass hammer	 	Very useful	 	and fair price
+User54	 	Wireless wires	 	Very useful	 	but expensive
+User55	 	Power balance 	 	Not so useful	 	and expensive
+User56	 	Solar clothe drying	 	Very useful	 	and fair price
+User57	 	White print toner	 	Very useful	 	but expensive
+User58	 	Glass hammer	 	Not so useful	 	and expensive
+User59	 	Wireless wires	 	Very useful	 	and fair price
+User60	 	Power balance 	 	Very useful	 	but expensive
+User61	 	Solar clothe drying	 	Not so useful	 	and expensive
+User62	 	White print toner	 	Very useful	 	and fair price
+User63	 	Glass hammer	 	Very useful	 	but expensive
+User64	 	Wireless wires	 	Not so useful	 	and expensive
+User65	 	Power balance 	 	Very useful	 	and fair price
+User66	 	Solar clothe drying	 	Very useful	 	but expensive
+User67	 	White print toner	 	Not so useful	 	and expensive
+User68	 	Glass hammer	 	Very useful	 	and fair price
+User69	 	Wireless wires	 	Very useful	 	but expensive
+User70	 	Power balance 	 	Not so useful	 	and expensive
+User71	 	Solar clothe drying	 	Very useful	 	and fair price
+User72	 	White print toner	 	Very useful	 	but expensive
+User73	 	Glass hammer	 	Not so useful	 	and expensive
+User74	 	Wireless wires	 	Very useful	 	and fair price
+User75	 	Power balance 	 	Very useful	 	but expensive
+User76	 	Solar clothe drying	 	Not so useful	 	and expensive
+User77	 	White print toner	 	Very useful	 	and fair price
+User78	 	Glass hammer	 	Very useful	 	but expensive
+User79	 	Wireless wires	 	Not so useful	 	and expensive
+User80	 	Power balance 	 	Very useful	 	and fair price
+User81	 	Solar clothe drying	 	Very useful	 	but expensive
+User82	 	White print toner	 	Not so useful	 	and expensive
+User83	 	Glass hammer	 	Very useful	 	and fair price
+User84	 	Wireless wires	 	Very useful	 	but expensive
+User85	 	Power balance 	 	Not so useful	 	and expensive
+User86	 	Solar clothe drying	 	Very useful	 	and fair price
+User87	 	White print toner	 	Very useful	 	but expensive
+User88	 	Glass hammer	 	Not so useful	 	and expensive
+User89	 	Wireless wires	 	Very useful	 	and fair price
+User90	 	Power balance 	 	Very useful	 	but expensive
+User91	 	Solar clothe drying	 	Not so useful	 	and expensive
+User92	 	White print toner	 	Very useful	 	and fair price
+User93	 	Glass hammer	 	Very useful	 	but expensive
+User94	 	Wireless wires	 	Not so useful	 	and expensive
+User95	 	Power balance 	 	Very useful	 	and fair price
+User96	 	Solar clothe drying	 	Very useful	 	but expensive
+User97	 	White print toner	 	Not so useful	 	and expensive
+User98	 	Glass hammer	 	Very useful	 	and fair price
+User99	 	Wireless wires	 	Very useful	 	but expensive
+User100	 	Power balance 	 	Not so useful	 	and expensive
